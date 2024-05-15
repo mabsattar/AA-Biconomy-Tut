d50440c9910e5bce91c42c67bc570849e3a52bb9
AbSattar <mdabdullsattar@gmail.com>
2024-05-15 05:51:31 +0530

    typos correct

--- src/components/Wallet.tsx
+++ src/components/Wallet.tsx
@@ -1,14 +1,14 @@
 import {useEffect, useRef, useState} from 'react'
 
 export default function Wallet() {
-    const sdkRef = usseRef<SocialLogin | null>(null);
+    const sdkRef = useRef<SocialLogin | null>(null);
     const [interval, enableInterval] = useState<boolean>(false);
 
     useEffect(() => {
         let configureLogin: NodeJS.Timeout | undefined;
         if (interval) {
             configureLogin = setInterval(() => {
-                if (!!sdjRef.current?.provider) {
+                if (!!sdkRef.current?.provider) {
                     clearInterval(configureLogin);
                 }
             }, 1000);
