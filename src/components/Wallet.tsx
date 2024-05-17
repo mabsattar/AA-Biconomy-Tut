2ccec59a5f731221d23762146604ad1048fc5bf3
AbSattar <mdabdullsattar@gmail.com>
2024-05-18 03:27:39 +0530

    initialized login function and setupSmartAccount

--- src/components/Wallet.tsx
+++ src/components/Wallet.tsx
@@ -1,4 +1,6 @@
-import {useEffect, useRef, useState} from 'react'
+import {useEffect, useRef, useState} from 'react';
+import { ethers } from "ethers";
+import { ChainId } from "@biconomy/core-types";
 
 export default function Wallet() {
     const sdkRef = useRef<SocialLogin | null>(null);
@@ -14,4 +16,58 @@ export default function Wallet() {
             }, 1000);
         }
     }, [interval]);
+
+    async function login() {
+        if (!sdkRef.current) {
+            const socialLoginSDK = new SocialLogin();
+            await socialLoginSDK.init({
+                chainId: ethers.utils.hexValue(chainId.POLYGON_MUMBAI).toString(),
+                network: "testnet",
+            });
+            sdkRef.current = socialLoginSDK;
+        }
+
+        if (!sdkRef.current.provider) {
+            sdkRef.current.showWallet();
+            enableInterval(true);
+        } else {
+            console.log("Hello");
+        }
+
+        async function setupSmartAccount() {
+            try {
+              if (!sdkRef.current?.provider) return;          
+              sdkRef.current.hideWallet();          
+              setLoading(true);          
+              let web3Provider = new ethers.providers.Web3Provider(
+                sdkRef.current?.provider
+              );
+              
+              setProvider(web3Provider);
+              const config: BiconomySmartAccountConfig = {
+                signer: web3Provider.getSigner(),
+                chainId: ChainId.POLYGON_MUMBAI,
+                bundler: bundler,
+                paymaster: paymaster,
+              };
+              const smartAccount = new BiconomySmartAccount(config);
+              await smartAccount.init();
+
+              setSmartAccount(smartAccount);
+            } catch (e) {
+              console.error(e);
+            }
+          
+            setLoading(false);
+          }
+    }
+
+      
+    
+      return (
+        <div>Wallet</div>
+      )
+    
+    
+    export default Wallet
 }
