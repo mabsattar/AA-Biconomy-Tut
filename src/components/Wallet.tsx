d63a6c1e981cacbbca5108a2af86a6666f875284
AbSattar <mdabdullsattar@gmail.com>
2024-05-22 21:48:45 +0530

    Wallet Transfer comp added

--- src/components/Wallet.tsx
+++ src/components/Wallet.tsx
@@ -2,6 +2,8 @@ import {useEffect, useRef, useState} from 'react';
 import { ethers } from "ethers";
 import { ChainId } from "@biconomy/core-types";
 import { BiconomySmartAccount, BiconomySmartAccountConfig } from "@biconomy/account";
+import { bundler, paymaster } from "@/constants";
+import { Fragment, useEffect, useRef, useState } from "react";
 
 export default function Wallet() {
     const sdkRef = useRef<SocialLogin | null>(null);
@@ -36,42 +38,82 @@ export default function Wallet() {
             enableInterval(true);
         } else {
             console.log("Hello");
+              setupSmartAccount();
         }
 
-        async function setupSmartAccount() {
-            try {
-              if (!sdkRef.current?.provider) return;          
-              sdkRef.current.hideWallet();          
-              setLoading(true);          
-              let web3Provider = new ethers.providers.Web3Provider(
-                sdkRef.current?.provider
-              );
+    async function setupSmartAccount() {
+        try {
+          if (!sdkRef.current?.provider) return;          
+          sdkRef.current.hideWallet();          
+          setLoading(true);          
+          let web3Provider = new ethers.providers.Web3Provider(
+            sdkRef.current?.provider
+          );
               
-              setProvider(web3Provider);
-              const config: BiconomySmartAccountConfig = {
-                signer: web3Provider.getSigner(),
-                chainId: ChainId.Sepolia,
-                bundler: bundler,
-                paymaster: paymaster,
-              };
-              const smartAccount = new BiconomySmartAccount(config);
-              await smartAccount.init();
+          setProvider(web3Provider);
+          const config: BiconomySmartAccountConfig = {
+            signer: web3Provider.getSigner(),
+            chainId: ChainId.Sepolia,
+            bundler: bundler,
+            paymaster: paymaster,
+          };
+          const smartAccount = new BiconomySmartAccount(config);
+          await smartAccount.init();
 
-              setSmartAccount(smartAccount);
-            } catch (e) {
-              console.error(e);
-            }
+          setSmartAccount(smartAccount);
+        } catch (e) {
+          console.error(e);
+        }
           
-            setLoading(false);
-          }
-    }
+        setLoading(false);
+      }
 
-      
+      async function logOut() {
+        await sdkRef.current?.logout();
+
+        sdkRef.current?.hiddenWallet();
+
+        setSmartAccount(undefined);
+        enableInterval(false);
+      }
+
+    }
+      ret  return (
+        <Fragment>
+          {/* Logout Button */}
+          {smartAccount && (
+            <button
+              onClick={logOut}
+              className="absolute right-0 m-3 rounded-lg bg-gradient-to-r from-green-400 to-blue-500 px-4 py-2 font-medium transition-all hover:from-green-500 hover:to-blue-600 "
+            >
+              Logout
+            </button>
+          )}
     
-      return (
-        <div>Wallet</div>
-      )
+          <div className="m-auto flex h-screen flex-col items-center justify-center gap-10 bg-gray-950">
+            <h1 className=" text-4xl text-gray-50 font-bold tracking-tight lg:text-5xl">
+              Send ERC20 using ERC20
+            </h1>
     
+            {/* Login Button */}
+            {!smartAccount && !loading && (
+              <button
+                onClick={login}
+                className="mt-10 rounded-lg bg-gradient-to-r from-green-400 to-blue-500 px-4 py-2 font-medium  transition-colors hover:from-green-500 hover:to-blue-600"
+              >
+                Login
+              </button>
+            )}
+    
+            {/* Loading state */}
+            {loading && <p>Loading account details...</p>}
+    
+            {smartAccount && (
+              <Fragment>{/* Add Transfer Component Here */}</Fragment>
+            )}
+          </div>
+        </Fragment>
+      );
     
     export default Wallet
 }
